fb5af9110331890dfa60de78b2170b19dc1a1845
alls7554 <alls970459@gmail.com>
2021-03-18 18:07:55 +0900

    server.js 정규식 수정, 약간의 예외처리

--- 2주차/js/cookie.js
+++ 2주차/js/cookie.js
@@ -1,17 +1,33 @@
 var setCookie = function (value) {
-        var user_name = $("#user_name").val();
-        document.cookie = user_name + '=' + value  + ';path=/';
-        location.href='./game.html';
+    var user_name = $("#user_name").val();
+
+    if(user_name == ""){
+        alert("nickname 설정");
+        return;
+    }
+
+    document.cookie = user_name + '=' + value  + ';path=/';
+    location.href='./game.html';
 };
 
 var getCookie = function() {
-        var value = document.cookie.split('=');
-        document.getElementById("my_name").innerHTML += value[0];
-        // document.getElementById("winner").innerHTML = value[0];
-        return value? value[0] : null;
-    };
+    var value = document.cookie.split('=');
+    
+    return value? value[0] : null;
+};
         
 var deleteCookie = function() {
     let name = document.getElementById("my_name").innerHTML;
     document.cookie = name + '=true; expires=Thu, 01 Jan 1999 00:00:10 GMT; path=/;';
+}
+
+var clearAllCookies = function(domain, path) {
+    var doc = document,
+        domain = domain || doc.domain,
+        path = path || '/',
+        cookies = doc.cookie.split(';'),
+        now = +(new Date);
+    for (var i = cookies.length - 1; i >= 0; i--) {
+      doc.cookie = cookies[i].split('=')[0] + '=; expires=Thu, 01 Jan 1999 00:00:10 GMT'; domain=' + domain + '; path=' + path';
+    }
 }
--- 2주차/server/server.js
+++ 2주차/server/server.js
@@ -1,52 +1,47 @@
 var http = require('http');
 var fs = require('fs');
 var path = require('path');
+
 var myPath = path.dirname(__filename);
 var rootPath = path.normalize(myPath + "/..")
 var _url = path.normalize(rootPath + "/pages/index.html")
 
 var app = http.createServer(function (request, response) {
 
-  var url = request.url;
-  var a = url.split('/');
-  // var isRootDir = false;
+    // 지정된 외 입력 방지를 위한 정규식
+    var excep_Re = new RegExp('(js|css)$');
 
-  if (a.length > 1 && a[1] != '') {
-    // console.log(a[1]);
-    //a[1] 나오는경우 1 기본 경로 / + 나온문자 (css ,js ,etc) 
-    //a[1] 나오는경우 2 favicon.ico // index.html 
-    var exe = a[1].split('.');
-
-    console.log('파일 확장자가 있나? ' + exe, exe.length, exe);
-    if (exe.length > 1) {
-
-      if (exe[0] == 'favicon') {
-        url = 404
-      } else {
-        url = rootPath + "/pages/" + url;
-        // url = path.normalize(rootPath + "/pages/" + url)
-        console.log(url)
-      }
-
+    var url = request.url;
+    var split_url = url.split('/');
+    
+    //url을 '/'을 기준으로 분리하였을 때, '' 나오는 경우와 그렇지 않은 경우가 있다.
+    //a[1]에 ''가 나오는경우      1 index.html 
+    //a[1]에 ''가 나오지않는 경우 2 기본 경로 / + 나온문자 (css ,js ,etc) 
+    if (split_url[1] != '') {
+        console.log(split_url)
+        var htmlEx = new RegExp('.*\.html$');
+        
+        // 확장자명이 html인 경우
+        if (htmlEx.test(split_url[1])) {
+            url = rootPath + "/pages/" + url;
+        } else if(excep_Re.test(split_url[1])){
+            url = rootPath + url;
+        } else {
+          url = rootPath+'/pages/404.html';
+          console.log(url)
+        }
     } else {
-      //url = 404
-      url = rootPath + "/" + exe + "/" + a[2];
-      // url = path.normalize(rootPath + "/" + exe + "/" + a[2])
-      console.log(url)
+        url = _url;
     }
 
-  } else {
-    url = _url;
-  }
-
-  if (url == 404) {
-    response.writeHead(404);
-    response.end();
-    return;
-  }
-  // console.log(url)
-  response.writeHead(200);
-  response.end(fs.readFileSync(url));
+    if (url == 404) {
+        response.writeHead(404);
+        response.end();
+        return;
+    }
+    
+    response.writeHead(200);
+    response.end(fs.readFileSync(url));
 });
 
 app.listen(3000);
